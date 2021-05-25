f808970d2f3fb8dbf551959e8d43c19625e6e32f
Karl Leswing <karl.leswing@gmail.com>
2021-05-25 10:19:59 -0400

    GOGOGO

--- devtools/jenkins.sh
+++ devtools/jenkins.sh
@@ -4,4 +4,14 @@ bash devtools/install.sh
 export PATH=`pwd`/anaconda/bin:$PATH
 source activate pml_qc
 
-python scripts/pml_qc.py
+# Untar the checkpoints
+cd checkpoint_files
+tar xzf checkpoint_PML_QC_CCSD.tar.gz
+cd ..
+
+# Grab condition files from https://searchworks.stanford.edu/view/kf921gd3855
+rsync -avz boltio:/nfs/working/deep_learn/key_store/pml_qc/minibatches_split_based_on_3rd_digit_9ha_batchsize16 ccsdt_dataset/
+
+python scripts/pml_qc.py --mode predict \
+                --checkpoint_dir checkpoint_files/checkpoint_PML_QC_CCSD \
+                --condition_files "ccsdt_dataset/minibatches_split_based_on_3rd_digit_9ha_batchsize16/testing2_conditions_batch95*.npy"
